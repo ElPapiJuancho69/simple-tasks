a920261280899d6e308ebe148fdcd2dfb606eed9
ElPapiJuancho69 <xdiaz69x@gmail.com>
2023-10-04 13:44:56 -0600

    Creacion mIGRACIONES

---
+++ database/migrations/2023_10_04_193713_create_tareas_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('tareas', function (Blueprint $table) {
+            $table->id(); // Equivalente a 'tarea_id serial PRIMARY KEY' en PostgreSQL
+            $table->string('titulo');
+            $table->string('descripcion');
+            $table->string('fecha_creacion');
+            $table->string('estado');
+            $table->unsignedBigInteger('usuario_id');
+            $table->foreign('usuario_id')->references('id')->on('usuarios');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('tareas');
+    }
+};
---
+++ database/migrations/2023_10_04_194258_create_actividades_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('actividades', function (Blueprint $table) {
+            Schema::create('actividades', function (Blueprint $table) {
+                $table->id(); // Equivalente a 'actividad_id serial PRIMARY KEY' en PostgreSQL
+                $table->string('fecha_actividad');
+                $table->string('descripcion_actividad');
+                $table->unsignedBigInteger('tarea_id');
+                $table->foreign('tarea_id')->references('id')->on('tareas');
+                $table->timestamps();
+            });
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('actividades');
+    }
+};
