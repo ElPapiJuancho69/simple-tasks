73809fcf65818c0d26107060c17242f5183cbb4f
ElPapiJuancho69 <xdiaz69x@gmail.com>
2023-12-06 22:34:57 -0600

    reportespdf

--- app/Http/Controllers/ReportesController.php
+++ app/Http/Controllers/ReportesController.php
@@ -52,7 +52,7 @@ class ReportesController extends Controller
     
     public function PDF()
     {
-        $reportes = reportes::all();
+        $reporte = reportes::all();
         $pdf    = PDF::loadView('PDF.listadoreportes', compact('reportes'));
         return $pdf->stream('listadoreportes.pdf');
     }
